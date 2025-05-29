110e917d983ed266aff20e04f4512357ec86e262
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2025-05-29 17:14:40 +0300

    Update index.js

--- packages/volto-footer/src/actions/index.js
+++ packages/volto-footer/src/actions/index.js
@@ -1,9 +1,15 @@
 import { GET_FOOTER_INHERIT } from '../constants';
 
-export const getFooterInherit = (url) => ({
-  type: GET_FOOTER_INHERIT,
-  request: {
-    op: 'get',
-    path: `${url}/@inherit?field=footer`,
-  },
-});
+export const getFooterInherit = (url) => {
+  let cleanedUrl = url;
+  if (url.endsWith('/edit')) {
+    cleanedUrl = url.slice(0, -'/edit'.length);
+  }
+  return {
+    type: GET_FOOTER_INHERIT,
+    request: {
+      op: 'get',
+      path: `${cleanedUrl}/@inherit?expand.inherit.behaviors=collective.volto.footer.editable`,
+    },
+  };
+};
